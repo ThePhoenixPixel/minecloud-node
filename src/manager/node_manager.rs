37d3626def29705383f15a6d3d73e095adaf490c
ThePhoenixPixel <thephoenix.private@gmail.com>
2026-02-19 10:28:01 +0100

    refator add new fn to node manager

--- src/manager/node_manager.rs
+++ src/manager/node_manager.rs
@@ -4,7 +4,7 @@ use tokio::sync::RwLock;
 use crate::api::cluster::{ClusterClient, RestClusterClient};
 use crate::config::CloudConfig;
 use crate::manager::service_manager::ServiceManager;
-use crate::types::{Service, Task};
+use crate::types::{EntityId, Service, ServiceRef, Task};
 use crate::utils::error::CloudResult;
 
 pub struct NodeManager {
@@ -22,10 +22,26 @@ impl NodeManager {
         })
     }
 
-    pub async fn stop_all_local_services(&self, msg: &str) {
-        todo!("Stop all local servcies");
+    pub async fn stop_all_local_services(&self, msg: &str) -> CloudResult<()> {
+        let services = self.service_manager.read().await.get_online_all().await;
+        for s in services {
+            self.stop_service(s.get_id().await, msg).await?;
+        }
+        Ok(())
     }
 
+    pub async fn stop_service(&self, id: EntityId, msg: &str) -> CloudResult<()> {
+        if let Some(service_ref) = self.service_manager.read().await.find_from_id(&id).await {
+            // service is local
+            self.unregistered_local_service(&service_ref).await?;
+            self.service_manager.write().await.stop_service(&id, msg).await?;
+
+        } else {
+            // service is remote
+            todo!("Send stop command to Other Node");
+        }
+        Ok(())
+    }
     pub async fn is_responsible_for_task(&self, task: &Task) -> bool {
         task.is_startup_local(&self.cloud_config)
     }
@@ -42,7 +58,7 @@ impl NodeManager {
     }
 
     pub async fn start_service_from_task(&self, task: &Task) -> CloudResult<()> {
-        if !(self.cloud_config.get_name() == self.find_best_node(task).await) {
+        if self.cloud_config.get_name() != self.find_best_node(task).await {
             // send start request to Node
             return Ok(())
         }
@@ -52,6 +68,60 @@ impl NodeManager {
             sm.get_or_create_service_ref(task).await?
         };
         self.service_manager.read().await.start(service_ref).await?;
+        Ok(())
+    }
+
+    pub async fn get_online_backend_server(&self) -> Vec<Service> {
+        let services = self.service_manager.read().await.get_online_backend_server().await;
+        let mut result = Vec::new();
+        for s in services {
+            result.push(s.read().await.get_service())
+        }
+        result
+    }
+
+    /// Local (Server Plugin called) -> info sent to Cluster
+    pub async fn on_local_service_registered(&self, id: EntityId) -> CloudResult<()> {
+        let service_ref = {
+            self.service_manager.read().await.get_from_id(&id).await?
+        };
+
+        self.service_manager.read().await.register_on_proxy(&service_ref.read().await.get_service()).await?;
+
+        Ok(())
+    }
+
+    async fn unregistered_local_service(&self, service_ref: &ServiceRef) -> CloudResult<()> {
+        self.service_manager.read().await.register_on_proxy(&service_ref.read().await.get_service()).await?;
+
+        Ok(())
+    }
+
+    /// Remote (Node called) -> info Local
+    pub async fn on_remote_service_registered(&self, service: Service) -> CloudResult<()> {
+        self.service_manager.read().await.register_on_proxy(&service).await?;
+        Ok(())
+    }
+
+    /// Local (Server Plugin called) -> info sent to Cluster
+    pub async fn on_local_service_shutdown(&self, id: EntityId) -> CloudResult<()> {
+        let service_ref = {
+            let sm = self.service_manager.read().await;
+            sm.get_from_id(&id).await?
+        };
+
+        if service_ref.read().await.is_shutdown_init() {
+            return Ok(());
+        }
+
+        self.unregistered_local_service(&service_ref).await?;
+
+        Ok(())
+    }
+
+    /// Remote (Node called) -> info Local
+    pub async fn on_remote_service_shutdown(&self, service: Service) -> CloudResult<()> {
+        self.service_manager.read().await.unregister_from_proxy(&service).await?;
         Ok(())
     }
 
