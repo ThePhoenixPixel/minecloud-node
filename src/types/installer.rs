95b2b122657b10beda42157b13a3e5a7b7c2d175
ThePhoenixPixel <thephoenix.private@gmail.com>
2026-03-19 19:13:35 +0000

    update Installer, impl From traits

--- src/types/installer.rs
+++ src/types/installer.rs
@@ -9,7 +9,7 @@ pub enum Installer {
 }
 
 impl Installer {
-    pub fn from(s: &str) -> Self {
+    pub fn from_str(s: &str) -> Self {
         match s {
             "InstallAll" => Installer::InstallAll,
             "InstallAllDesc" => Installer::InstallAllDesc,
@@ -18,15 +18,37 @@ impl Installer {
             _ => Installer::InstallAll,
         }
     }
-}
 
-impl Into<&str> for Installer {
-    fn into(self) -> &'static str {
-        match self {
+    pub fn too_str(value: &Installer) -> &str {
+        match value {
             Installer::InstallAll => "InstallAll",
             Installer::InstallAllDesc => "InstallAllDesc",
             Installer::InstallRandom => "InstallRandom",
             Installer::InstallRandomWithPriority => "InstallRandomWithPriority",
         }
+    }
+}
+
+impl From<&str> for Installer {
+    fn from(s: &str) -> Self {
+        Installer::from_str(s)
+    }
+}
+
+impl From<String> for Installer {
+    fn from(s: String) -> Self {
+        Installer::from_str(s.as_str())
+    }
+}
+
+impl From<Installer> for String {
+    fn from(value: Installer) -> Self {
+       Installer::too_str(&value).to_string()
+    }
+}
+
+impl From<&Installer> for String {
+    fn from(value: &Installer) -> Self {
+        Installer::too_str(value).to_string()
     }
 }
